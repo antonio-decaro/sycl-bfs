9a1894695ea8c24005c547a7ae054bb1c6215445
antonio-decaro <antonio.decaro99@outlook.it>
2023-10-04 15:38:59 +0200

    arg parse change

--- include/arg_parse.hpp
+++ include/arg_parse.hpp
@@ -3,7 +3,7 @@
 
 // read the graph from the file
 bool check_args(int& argc, char**& argv) {
-    if (argc != 2) {
+    if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <graph_path> [out_data]" << std::endl;
         return false;
     }
