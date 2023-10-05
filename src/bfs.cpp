9d036050ffd3cb75358e39dd54a1db8dd8c57c5e
antonio-decaro <antonio.decaro99@outlook.it>
2023-10-05 14:39:51 +0200

    removed write back

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -22,7 +22,64 @@ void copy_data(SYCL_GraphData& sycl_data, HostData& data) {
     }
 }
 
-void sm_BFS(sycl::queue& queue, SYCL_GraphData& data, std::vector<sycl::event>& events) {
+// TODO to fix
+void single_queue_BFS(sycl::queue& queue, SYCL_GraphData& data, std::vector<sycl::event>& events) {
+
+    int* changed = s::malloc_device<int>(1, queue);
+    queue.fill(changed, false, 1);
+    int* level = s::malloc_shared<int>(1, queue);
+    queue.fill(level, 0, 1);
+
+    s::range<1> global{data.num_nodes + (WORK_GROUP_SIZE - (data.num_nodes % WORK_GROUP_SIZE))};
+    s::range<1> local{WORK_GROUP_SIZE};
+
+    {
+        auto e = queue.submit([&](sycl::handler& h) {
+            auto offsets_acc = data.offsets.get_access<s::access::mode::read>(h);
+            auto edges_acc = data.edges.get_access<s::access::mode::read>(h);
+            auto distances_acc = data.distances.get_access<s::access::mode::read_write>(h);
+            auto parents_acc = data.parents.get_access<s::access::mode::discard_write>(h);
+
+            s::stream out(256, 16, h);
+
+            h.parallel_for(s::nd_range<1>{global, local}, [=, num_nodes=data.num_nodes](s::nd_item<1> idx) {
+                
+                s::atomic_ref<int, s::memory_order::acq_rel, s::memory_scope::device, s::access::address_space::global_space> changed_ref {*changed};
+
+                out << "Init\n";
+                do {
+                    int node = idx.get_global_linear_id();
+                    changed_ref.store(0);
+                    if (node < num_nodes && distances_acc[node] == *level) {
+                        for (int i = offsets_acc[node]; i < offsets_acc[node + 1]; i++) {
+                            int neighbor = edges_acc[i];
+                            if (distances_acc[neighbor] == -1) {
+                                distances_acc[neighbor] = *level + 1;
+                                parents_acc[neighbor] = node;
+                                changed_ref.store(1);
+                            }
+                        }
+                    }
+                    out << "Before\n";
+
+                    idx.barrier(s::access::fence_space::global_space);
+                    if (node == 0) {
+                        (*level)++;
+                    }
+                    idx.barrier(s::access::fence_space::global_space);
+                    out << "After\n";
+                } while (changed_ref.load());
+                out << "I'm out " << idx.get_global_linear_id() << "\n";
+            });
+        });
+        events.push_back(e);
+        e.wait();
+    }
+
+    std::cout << "[*] Max depth reached: " << *level << std::endl;
+}
+
+void multi_events_BFS(sycl::queue& queue, SYCL_GraphData& data, std::vector<sycl::event>& events) {
 
     bool* changed = s::malloc_shared<bool>(1, queue);
     queue.fill(changed, false, 1);
@@ -69,7 +126,8 @@ void SimpleBFS::run() {
     std::vector<sycl::event> events;
 
     auto start_glob = std::chrono::high_resolution_clock::now();
-    sm_BFS(queue, sycl_data, events);
+    multi_events_BFS(queue, sycl_data, events);
+    // single_queue_BFS(queue, sycl_data, events);
     auto end_glob = std::chrono::high_resolution_clock::now();
 
     long duration = 0;
@@ -78,6 +136,8 @@ void SimpleBFS::run() {
         auto end = e.get_profiling_info<sycl::info::event_profiling::command_end>();
         duration += (end - start);
     }
+
+    copy_data(sycl_data, data); 
 
     std::cout << "[*] Kernels duration: " << duration / 1000 << " us" << std::endl;
     std::cout << "[*] Total duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end_glob - start_glob).count() << " us" << std::endl;
